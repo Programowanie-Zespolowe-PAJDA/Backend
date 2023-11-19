d8ddd8da96bdc9e87df30f79a2a0dce073e98847
Nekio21 <108297802+Nekio21@users.noreply.github.com>
2023-11-19 16:34:45 +0100

    Init projekt

---
+++ src/main/java/umk/mat/pajda/ProjektZespolowy/ProjektZespolowyApplication.java
@@ -0,0 +1,13 @@
+package umk.mat.pajda.ProjektZespolowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektZespolowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektZespolowyApplication.class, args);
+	}
+
+}

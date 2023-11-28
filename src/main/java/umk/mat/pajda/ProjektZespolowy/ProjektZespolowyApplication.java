b9d5a0512feb286a06aef9a55f3696bc6eb65cb6
DarthRevan77797 <general77779@gmail.com>
2023-11-28 20:02:19 +0100

    [feature] Added springdoc autodoc for backend api v2

--- src/main/java/umk/mat/pajda/ProjektZespolowy/ProjektZespolowyApplication.java
+++ src/main/java/umk/mat/pajda/ProjektZespolowy/ProjektZespolowyApplication.java
@@ -8,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Kellner app",
-		description = "An application that allows the waiter to receive tips electronically and assess his skills. Backend API Documentation ",
+		description = "An application that allows the waiter to receive tips electronically and assess his skills." +
+				" Backend API Documentation ",
 		version = "v0.1"))
 public class ProjektZespolowyApplication {
 
--- src/main/java/umk/mat/pajda/ProjektZespolowy/controllers/ExampleEndpointClass.java
+++ src/main/java/umk/mat/pajda/ProjektZespolowy/controllers/ExampleEndpointClass.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@Tag(name = "Example Endpoint", description = "Controller for handling ExampleEndpoint API requests")
+@Tag(name = "Example Endpoint",
+        description = "Controller for handling ExampleEndpoint API requests")
 public class ExampleEndpointClass {
 
     @GetMapping("/hello")
     @ResponseBody
     @Operation(summary = "Get hellow", description = "Returns hellow")
-    public String sayHello(){
+    public String sayHello() {
         return "hello";
     }
 }

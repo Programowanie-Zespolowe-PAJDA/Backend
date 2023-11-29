5425ca3744d0a116511bd78a723b74ee9641158c
DarthRevan77797 <general77779@gmail.com>
2023-11-29 21:06:09 +0100

    [feature] Added springdoc autodoc for backend api v4

--- src/main/java/umk/mat/pajda/ProjektZespolowy/ProjektZespolowyApplication.java
+++ src/main/java/umk/mat/pajda/ProjektZespolowy/ProjektZespolowyApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "KellnerApp", description = "Doc"))
+@OpenAPIDefinition(info = @Info(title = "Kellner app",
+        description = "An application that allows the waiter to receive tips electronically and assess his skills." +
+                "and assess his skills. Backend API Documentation ",
+        version = "v0.1"))
 public class ProjektZespolowyApplication {
   public static void main(String[] args) {
     SpringApplication.run(ProjektZespolowyApplication.class, args);

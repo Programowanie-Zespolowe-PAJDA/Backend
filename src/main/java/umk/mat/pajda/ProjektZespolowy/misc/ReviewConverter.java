9bf523268a8a514f5c8a7adc0e50b252ca34f368
Nekio21 <108297802+Nekio21@users.noreply.github.com>
2024-01-11 03:48:31 +0100

    make a little fix in ReviewConverter(formating check)

--- src/main/java/umk/mat/pajda/ProjektZespolowy/misc/ReviewConverter.java
+++ src/main/java/umk/mat/pajda/ProjektZespolowy/misc/ReviewConverter.java
@@ -7,46 +7,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import umk.mat.pajda.ProjektZespolowy.DTO.ReviewDTO;
 import umk.mat.pajda.ProjektZespolowy.entity.Review;
-import umk.mat.pajda.ProjektZespolowy.entity.User;
 
 @Component
 public class ReviewConverter {
 
-  private final UserConverter userConverter;
+    private final UserConverter userConverter;
 
-  @Autowired
-  public ReviewConverter(UserConverter userConverter){
-      this.userConverter = userConverter;
-      userConverter.setReviewConverter(this);
-  }
+    @Autowired
+    public ReviewConverter(UserConverter userConverter){
+        this.userConverter = userConverter;
+        userConverter.setReviewConverter(this);
+    }
 
-  public ReviewDTO createDTO(Review source) {
-    ReviewDTO res = new ReviewDTO();
-    res.setId(source.getId());
-    res.setRating(source.getRating());
-    res.setComment(source.getComment());
-    // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-    res.setReviewTimeStamp(source.getReviewTimeStamp());
-    res.setUser(userConverter.createDTO(source.getUser()));
-    res.setClientName(source.getClientName());
-    res.setHashRevID(source.getHashRevID());
-    return res;
-  }
+    public ReviewDTO createDTO(Review source) {
+        ReviewDTO res = new ReviewDTO();
+        res.setId(source.getId());
+        res.setRating(source.getRating());
+        res.setComment(source.getComment());
 
-  public Review createEntity(ReviewDTO source) {
-    Review res = new Review();
-    res.setId(source.getId());
-    res.setRating(source.getRating());
-    res.setComment(source.getComment());
-    res.setReviewTimeStamp(source.getReviewTimeStamp());
-    res.setUser(userConverter.createEntity(source.getUser()));
-    res.setClientName(source.getClientName());
-    res.setHashRevID(source.getHashRevID());
-    return res;
-  }
+        // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        res.setReviewTimeStamp(source.getReviewTimeStamp());
+        res.setUser(userConverter.createDTO(source.getUser()));
+        res.setClientName(source.getClientName());
+        res.setHashRevID(source.getHashRevID());
 
-  public List<ReviewDTO> createReviewDTOList(List<Review> list) {
-    List<ReviewDTO> listDTO = list.stream().map(this::createDTO).collect(Collectors.toList());
-    return listDTO;
-  }
+        return res;
+    }
+
+    public Review createEntity(ReviewDTO source) {
+        Review res = new Review();
+        res.setId(source.getId());
+        res.setRating(source.getRating());
+        res.setComment(source.getComment());
+        res.setReviewTimeStamp(source.getReviewTimeStamp());
+        res.setUser(userConverter.createEntity(source.getUser()));
+        res.setClientName(source.getClientName());
+        res.setHashRevID(source.getHashRevID());
+        return res;
+    }
+
+    public List<ReviewDTO> createReviewDTOList(List<Review> list) {
+        List<ReviewDTO> listDTO = list.stream().map(this::createDTO).collect(Collectors.toList());
+        return listDTO;
+    }
 }

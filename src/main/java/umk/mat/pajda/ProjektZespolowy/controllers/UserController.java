5f8f3d561aadbc924a257f8aec711953acfd6271
DarthRevan77797 <general77779@gmail.com>
2024-01-16 22:20:51 +0100

    [fix] codeStyle

--- src/main/java/umk/mat/pajda/ProjektZespolowy/controllers/UserController.java
+++ src/main/java/umk/mat/pajda/ProjektZespolowy/controllers/UserController.java
@@ -1,94 +1,81 @@
 package umk.mat.pajda.ProjektZespolowy.controllers;
 
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import umk.mat.pajda.ProjektZespolowy.DTO.ReviewDTO;
 import umk.mat.pajda.ProjektZespolowy.DTO.UserDTO;
 import umk.mat.pajda.ProjektZespolowy.services.UserService;
-
-import java.util.List;
 
 @RequestMapping("/user")
 @RestController
 @Tag(
-        name = "User Endpoints",
-        description = "Controller for handling requests related to add/del/patch/read users")
+    name = "User Endpoints",
+    description = "Controller for handling requests related to add/del/patch/read users")
 public class UserController {
 
-    private final UserService userService;
+  private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @PostMapping("/add")
+  @Operation(summary = "POST - Add \"new User\"", description = "Following endpoint adds new User")
+  public ResponseEntity<String> addNewReview(@RequestBody UserDTO userDTO) {
+    if (userService.addUser(userDTO)) {
+      return ResponseEntity.status(HttpStatus.CREATED).body("adding successful");
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("adding failed");
     }
+  }
 
-    @PostMapping("/add")
-    @Operation(
-            summary = "POST - Add \"new User\"",
-            description = "Following endpoint adds new User")
-    public ResponseEntity<String> addNewReview(@RequestBody UserDTO userDTO) {
-        if (userService.addUser(userDTO)) {
-            return ResponseEntity.status(HttpStatus.CREATED).body("adding successful");
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("adding failed");
-        }
+  @PatchMapping("/patch")
+  @Operation(
+      summary = "PATCH - modify \"User\"",
+      description = "Following endpoint modifies a User")
+  public ResponseEntity<String> modReview(@RequestBody UserDTO userDTO) {
+    if (userService.getUser(userDTO.getId()) != null) {
+      if (userService.patchSelectedUser(userDTO)) {
+        return ResponseEntity.status(HttpStatus.OK).body("modifying successful");
+      } else {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("modifying failed");
+      }
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("modifying failed");
     }
+  }
 
-
-    @PatchMapping("/patch")
-    @Operation(
-            summary = "PATCH - modify \"User\"",
-            description = "Following endpoint modifies a User")
-    public ResponseEntity<String> modReview(@RequestBody UserDTO userDTO) {
-        if (userService.getUser(userDTO.getId()) != null) {
-            if (userService.patchSelectedUser(userDTO)) {
-                return ResponseEntity.status(HttpStatus.OK).body("modifying successful");
-            } else {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("modifying failed");
-            }
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("modifying failed");
-        }
+  @DeleteMapping("/del/{id}")
+  @Operation(
+      summary = "DELETE - delete \"User\"",
+      description = "Following endpoint deletes a User of id")
+  public ResponseEntity<String> delReview(@PathVariable int id) {
+    if (userService.deleteSelectedUser(id)) {
+      return ResponseEntity.status(HttpStatus.OK).body("delete successful");
+    } else {
+      return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("delete failed");
     }
+  }
 
+  @GetMapping("/get")
+  @Operation(summary = "Get - get all \"User\"", description = "Following endpoint returns a User")
+  public ResponseEntity<List<UserDTO>> readReview() {
+    return ResponseEntity.status(HttpStatus.OK).body(userService.getAllUsers());
+  }
 
-
-    @DeleteMapping("/del/{id}")
-    @Operation(
-            summary = "DELETE - delete \"User\"",
-            description = "Following endpoint deletes a User of id")
-    public ResponseEntity<String> delReview(@PathVariable int id) {
-        if (userService.deleteSelectedUser(id)) {
-            return ResponseEntity.status(HttpStatus.OK).body("delete successful");
-        } else {
-            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("delete failed");
-        }
+  @GetMapping("/get/{id}")
+  @Operation(
+      summary = "Get - get \"User\"",
+      description = "Following endpoint returns a User of id")
+  public ResponseEntity<UserDTO> readReview(@PathVariable int id) {
+    UserDTO ret = userService.getUser(id);
+    if (ret != null) {
+      return ResponseEntity.status(HttpStatus.OK).body(ret);
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
-
-
-    @GetMapping("/get")
-    @Operation(
-            summary = "Get - get all \"User\"",
-            description = "Following endpoint returns a User")
-    public ResponseEntity<List<UserDTO>> readReview() {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.getAllUsers());
-    }
-
-
-    @GetMapping("/get/{id}")
-    @Operation(
-            summary = "Get - get \"User\"",
-            description = "Following endpoint returns a User of id")
-    public ResponseEntity<UserDTO> readReview(@PathVariable int id) {
-        UserDTO ret = userService.getUser(id);
-        if (ret != null) {
-            return ResponseEntity.status(HttpStatus.OK).body(ret);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-    }
-
+  }
 }
--- src/main/java/umk/mat/pajda/ProjektZespolowy/services/UserService.java
+++ src/main/java/umk/mat/pajda/ProjektZespolowy/services/UserService.java
@@ -1,71 +1,65 @@
 package umk.mat.pajda.ProjektZespolowy.services;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import umk.mat.pajda.ProjektZespolowy.DTO.ReviewDTO;
 import umk.mat.pajda.ProjektZespolowy.DTO.UserDTO;
-import umk.mat.pajda.ProjektZespolowy.entity.Review;
 import umk.mat.pajda.ProjektZespolowy.entity.User;
 import umk.mat.pajda.ProjektZespolowy.misc.UserConverter;
 import umk.mat.pajda.ProjektZespolowy.repository.UserRepository;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-
 @Service
 public class UserService {
-    private final UserConverter userConverter;
-    private final UserRepository userRepository;
+  private final UserConverter userConverter;
+  private final UserRepository userRepository;
 
-    @Autowired
-    public UserService(UserConverter userConverter, UserRepository userRepository) {
-        this.userConverter = userConverter;
-        this.userRepository = userRepository;
+  @Autowired
+  public UserService(UserConverter userConverter, UserRepository userRepository) {
+    this.userConverter = userConverter;
+    this.userRepository = userRepository;
+  }
+
+  public boolean addUser(UserDTO userDTO) {
+    try {
+      userDTO.setId(0);
+      userRepository.save(userConverter.createEntity(userDTO));
+    } catch (Exception e) {
+      return false;
+    }
+    return true;
+  }
+
+  public List<UserDTO> getAllUsers() {
+    return userConverter.createUserDTOList(userRepository.findAll());
+  }
+
+  public UserDTO getUser(int id) {
+    User returnData = null;
+    try {
+      returnData = userRepository.findById(id).get();
+    } catch (NoSuchElementException e) {
+      return null;
     }
 
+    return userConverter.createDTO(returnData);
+  }
 
-    public boolean addUser(UserDTO userDTO) {
-        try {
-            userDTO.setId(0);
-            userRepository.save(userConverter.createEntity(userDTO));
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
+  public boolean deleteSelectedUser(int id) {
+    try {
+      userRepository.deleteById(id);
+    } catch (Exception e) {
+      return false;
     }
+    return true;
+  }
 
-    public List<UserDTO> getAllUsers() {
-        return userConverter.createUserDTOList(userRepository.findAll());
+  public boolean patchSelectedUser(UserDTO userDTO) {
+    try {
+      userRepository.save(userConverter.createEntity(userDTO));
+    } catch (Exception e) {
+      return false;
     }
-
-    public UserDTO getUser(int id) {
-        User returnData = null;
-        try {
-            returnData = userRepository.findById(id).get();
-        } catch (NoSuchElementException e) {
-            return null;
-        }
-
-        return userConverter.createDTO(returnData);
-    }
-
-    public boolean deleteSelectedUser(int id) {
-        try {
-            userRepository.deleteById(id);
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
-    }
-
-    public boolean patchSelectedUser(UserDTO userDTO) {
-        try {
-            userRepository.save(userConverter.createEntity(userDTO));
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
-    }
-
-
+    return true;
+  }
 }

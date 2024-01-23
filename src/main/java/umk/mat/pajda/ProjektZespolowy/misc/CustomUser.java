582856966bd1232a13bda94b6e2e19e2a26c5818
Adrian Politowski <politowskiadrian78@gmail.com>
2024-01-23 16:30:46 +0100

    add login by user entity

---
+++ src/main/java/umk/mat/pajda/ProjektZespolowy/misc/CustomUser.java
@@ -0,0 +1,54 @@
+package umk.mat.pajda.ProjektZespolowy.misc;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUser implements UserDetails {
+    private String mail;
+    private String password;
+    private String role;
+
+    public CustomUser(String mail, String password, String role) {
+        this.mail = mail;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(() -> role);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return mail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

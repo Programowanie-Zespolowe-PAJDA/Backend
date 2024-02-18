015154d29cfba3613f8a87ee4b1f160993000b7c
DarthRevan77797 <general77779@gmail.com>
2024-02-18 17:19:31 +0100

    [tests] add tests for following controllers User and Review

---
+++ src/test/java/umk/mat/pajda/ProjektZespolowy/Controllers/UserControllerTest.java
@@ -0,0 +1,118 @@
+package umk.mat.pajda.ProjektZespolowy.Controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import umk.mat.pajda.ProjektZespolowy.configs.JwtAuthenticationFilter;
+import umk.mat.pajda.ProjektZespolowy.controllers.UserController;
+import umk.mat.pajda.ProjektZespolowy.services.JWTService;
+import umk.mat.pajda.ProjektZespolowy.services.UserService;
+import umk.mat.pajda.ProjektZespolowy.services.impl.AuthenticationServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+@AutoConfigureMockMvc
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @MockBean private JWTService jwtService;
+
+    @InjectMocks
+    private AuthenticationServiceImpl authenticationService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void userControllerTest_getAllUsers_status() throws Exception {
+        mockMvc.perform(get("/user")).andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void userControllerTest_getAllUsers_IsValidJson() throws Exception {
+        ResultActions result = mockMvc.perform(get("/user"));
+        String content = result.andReturn().getResponse().getContentAsString();
+        // Asserting that the content is valid JSON
+        assertDoesNotThrow(() -> objectMapper.readTree(content));
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void userControllerTest_getOwner_status() throws Exception {
+        mockMvc.perform(get("/user/profile")).andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void userControllerTest_getOwner_IsValidJson() throws Exception {
+        ResultActions result = mockMvc.perform(get("/user/profile"));
+        String content = result.andReturn().getResponse().getContentAsString();
+        // Asserting that the content is valid JSON
+        assertDoesNotThrow(() -> objectMapper.readTree(content));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void userControllerTest_getSelectedUser_status() throws Exception {
+        mockMvc.perform(get("/user/1")).andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void userControllerTest_getSelectedUser_IsValidJson() throws Exception {
+        ResultActions result = mockMvc.perform(get("/user/1"));
+        String content = result.andReturn().getResponse().getContentAsString();
+        // Asserting that the content is valid JSON
+        assertDoesNotThrow(() -> objectMapper.readTree(content));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void userControllerTest_delUser_status() throws Exception {
+        //When
+        when(userService.deleteSelectedUser(any(int.class))).thenReturn(true);
+        //Then
+        mockMvc.perform(delete("/user/1")).andExpect(status().isOk());
+
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void userControllerTest_modInformationsOfUser_status() throws Exception {
+        mockMvc.perform(patch("/user/editInformations")).andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void userControllerTest_modPasswordOfUser_status() throws Exception {
+        mockMvc.perform(patch("/user/editPassword")).andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void userControllerTest_modEmailOfUser_status() throws Exception {
+        mockMvc.perform(patch("/user/editEmail")).andExpect(status().isOk());
+    }
+}
